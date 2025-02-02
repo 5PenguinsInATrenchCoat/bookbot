b83d877e139f548e0f295260b005707a58b8b27e
5PenguinsInATrenchCoat <draven2121@gmail.com>
2025-02-01 21:45:11 -0800

    Cleaned up code.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 def main():
+    book_path = "books/frankenstein.txt"
+    text = get_text(book_path)
+    word_count =get_num_words(text)
+    print(f"{word_count} words found in text.\n ")
+    
 
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+def get_text(book_path):
+    with open(book_path) as f:
+        text = f.read()
+        return text
     #opens text, then sets file_contents to the full output
 
-    print(file_contents)
-
-
+def get_num_words(text):
+    word_count = len(text.split())
+    return word_count
+    #uses list to count total words
+    
+    
+    
+    
 if __name__ == "__main__":
     main()
