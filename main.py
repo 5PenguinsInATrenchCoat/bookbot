a0dc544889f958b293c8dc6a295b6efef2c3e100
5PenguinsInATrenchCoat <draven2121@gmail.com>
2025-02-02 00:23:49 -0800

    Final Submission

--- main.py
+++ main.py
@@ -4,31 +4,32 @@ def main():
     word_count = get_num_words(text)
     char_count = get_char_count(text)
     print(f"{word_count} words found in text.\n")
-    #print(f"Space count: {char_count.get(' ', 0)}")
-    #for char in char_count:
-    #    print(f"'{char}'" + ": " + f"{char_count[char]}") 
-    print(char_count)
+    for char in char_count:
+        print(f"The '{char}' character was found {char_count[char]} times.") 
+    print("--- End Report ---")
+
 
 def get_text(book_path):
     with open(book_path) as f:
         text = f.read()
         return text
-    #opens text, then sets file_contents to the full output
+    # opens text, then sets file_contents to the full output
 
 def get_num_words(text):
     word_count = len(text.split())
     return word_count
-    #uses list to count total words
+    # uses list to count total words
     
 def get_char_count(text):
-    lower_Text = text.lower()
-    lines = lower_Text.splitlines()
     char_count = {}
+    lower_Text = text.lower()
     for char in lower_Text:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
+        if char.isalpha():
+        # only if the character is alphabetical
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
     return char_count
             
 
