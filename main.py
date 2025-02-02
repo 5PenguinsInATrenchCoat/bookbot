74bf135904a229702de9dd39ff63bf58a3d81a82
5PenguinsInATrenchCoat <draven2121@gmail.com>
2025-02-02 00:14:27 -0800

    Added character count.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
-    word_count =get_num_words(text)
-    print(f"{word_count} words found in text.\n ")
-    
+    word_count = get_num_words(text)
+    char_count = get_char_count(text)
+    print(f"{word_count} words found in text.\n")
+    #print(f"Space count: {char_count.get(' ', 0)}")
+    #for char in char_count:
+    #    print(f"'{char}'" + ": " + f"{char_count[char]}") 
+    print(char_count)
 
 def get_text(book_path):
     with open(book_path) as f:
@@ -16,7 +20,18 @@ def get_num_words(text):
     return word_count
     #uses list to count total words
     
-    
+def get_char_count(text):
+    lower_Text = text.lower()
+    lines = lower_Text.splitlines()
+    char_count = {}
+    for char in lower_Text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+            
+
     
     
 if __name__ == "__main__":
